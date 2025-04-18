265a1cfef1725655225b9d60e4dec6960104b0c4
Zoni Stefano <128632017+zonistefano@users.noreply.github.com>
2025-04-18 22:25:52 +0000

    Release version=1.0.3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       baseUrl: "http://localhost:3000",
-      appVersion: "1.0.2",
+      appVersion: "1.0.3",
       kpi: {
         hr: "",
       },
